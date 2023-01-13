cd051b7fcd7cc25d3085b1b5b88e584394f91410
Sushant Guha <sush.guha@gmail.com>
2023-01-12 21:00:36 -0500

    Addparty endpoint

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,33 +1,93 @@
 import React from "react";
 import { Container, Row, Button, Form } from "react-bootstrap";
 import { useState } from "react";
-import { UserAuth } from '../context/AuthContext';
+import { UserAuth } from "../context/AuthContext";
+import { projDB } from "../firebase/config.js";
+import { collection, query, getDocs, addDoc } from "firebase/firestore";
+import { timestamp } from "../firebase/config";
+
+const codeGen = (length) => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 const Landing = () => {
-  const {user, logout} = UserAuth();
+  const { user, logout } = UserAuth();
   const [entered, setEntered] = useState("");
   const handleSignOut = async () => {
-    try{
-        await logout();
+    try {
+      await logout();
+    } catch (error) {
+      console.log(error);
     }
-    catch (error){
-    console.log(error);
-}}
+  };
+
+  const createParty = async (e) => {
+    e.preventDefault();
+    // Starts a listening session.
+
+    // Generate random code and check if code is already present in the database
+    let partyCode = codeGen(6);
+    let isPresent = false;
+    do {
+      if (isPresent) {
+        isPresent = false;
+        partyCode = codeGen(6);
+      }
+      // Checks if the code is already in the db
+      const q = query(collection(projDB, "parties"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data().currentSong);
+        if (doc.data().partyCode === partyCode) {
+          isPresent = true;
+          console.log("IS PRESENT");
+        }
+      });
+    } while (isPresent);
+
+    // adds a party to the firestore database
+    const partyInfo = {
+      partyCode: partyCode,
+      partyTime: timestamp.now(),
+      hostUsername: user.displayName,
+      members: [user.displayName],
+      songs: [],
+      currentSong: "",
+      numberActive: 1,
+    };
+
+    const docRef = await addDoc(collection(projDB, "parties"), partyInfo);
+    console.log("Party officially added!");
+
+    // Navigate to the party dashboard page
+    // The call firebase to get the party info from that page.
+  };
+
   return (
-    
     <Container
       className="container-containerState  hover"
       style={{ height: "100vh " }}
     >
       <Row>
-       <p>Welcome ,{user?.displayName}</p>
+        <p>Welcome ,{user?.displayName}</p>
       </Row>
       <br></br>
       <Row>
-       <button onClick = {handleSignOut} className = 'border py-2 px-5 mt-10'>Logout from Google</button>
+        <button onClick={handleSignOut} className="border py-2 px-5 mt-10">
+          Logout from Google
+        </button>
       </Row>
       <br></br>
       <Row>
-        <Button  variant="success">
+        <Button variant="success" onClick={createParty}>
           Start a Listening Session
         </Button>{" "}
       </Row>
@@ -35,7 +95,7 @@ const Landing = () => {
       <Row>
         <Form>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Party Code</Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter Party Code"
@@ -45,11 +105,7 @@ const Landing = () => {
             />
           </Form.Group>
 
-          <Button
-            variant="primary"
-            
-            type="submit"
-          >
+          <Button variant="primary" type="submit">
             Join the Session!
           </Button>
         </Form>
