71a6803cf7861d5b3e8b36eec17624f883e6f8f4
Sushant Guha <sush.guha@gmail.com>
2023-01-15 00:03:26 -0500

    getParty fixed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,16 @@
-import React from "react";
 import { Container, Row, Button, Form } from "react-bootstrap";
 import { useState } from "react";
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import { projDB } from "../firebase/config.js";
-import { collection, query, getDocs, addDoc } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  query,
+  getDocs,
+  addDoc,
+  setDoc,
+} from "firebase/firestore";
 import { timestamp } from "../firebase/config";
 
 const codeGen = (length) => {
@@ -66,66 +72,87 @@ const Landing = () => {
     };
 
     const docRef = await addDoc(collection(projDB, "parties"), partyInfo);
-    const q = query(collection(projDB, "users"));
-      const querySnapshot = await getDocs(q);
-      let userPresent=false;
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data().currentSong);
-        if (doc.data().Name === user.displayName) {
-          userPresent = true;
-          console.log("IS PRESENT");
-        }
-      });
-      if(!userPresent){
-        const docRef = await addDoc(collection(projDB, "users"), user.displayName);
-        
+    const q = query(collection(projDB, "parties"));
+    const querySnapshot = await getDocs(q);
+    let userPresent = false;
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      // console.log(doc.id, " => ", doc.data().currentSong);
+      if (doc.data().Name === user.displayName) {
+        userPresent = true;
+        console.log("IS PRESENT");
       }
-    
-    window.location.href="/party/"+partyCode;
+    });
+    if (!userPresent) {
+      // const docRef = await addDoc(
+      //   collection(projDB, "users"),
+      //   user.displayName
+      // );
+    }
+
+    window.location.href = "/party/" + partyCode;
     // Navigate to the party dashboard page
     // The call firebase to get the party info from that page.
   };
 
-  const getParty = async(e) => {
-    let partyCode = "123456";
-    let userName = "tarun";
+  const getParty = async (e) => {
+    e.preventDefault();
+    let partyCode = entered;
+    let userName = user.displayName;
     let isPresent = false;
+    let foundParty = null;
+
     const q = query(collection(projDB, "parties"));
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data().currentSong);
-        if (doc.data().partyCode === partyCode) {
-          isPresent = true;
-          console.log("IS PRESENT");
-        }
-      });
-    if(isPresent){
-      //check if username is in the party if so add oatherwise do not add
-      const q = query(collection(projDB, "users"));
-      const querySnapshot = await getDocs(q);
-      let userPresent=false;
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data().currentSong);
-        if (doc.data().Name === userName) {
-          userPresent = true;
-          console.log("IS PRESENT");
-        }
-      });
-      if(!userPresent){
-        const docRef = await addDoc(collection(projDB, "users"), userName);
-        
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.data());
+      // doc.data() is never undefined for query doc snapshots
+      // console.log(doc.id, " => ", doc.data().currentSong);
+      if (doc.data().partyCode === partyCode) {
+        isPresent = true;
+        console.log("IS PRESENT");
+        foundParty = doc;
       }
-    }
-    else{
+    });
+    if (isPresent) {
+      //check if username is in the party if so add oatherwise do not add
+      // const q = query(collection(projDB, "parties"));
+      // const querySnapshot = await getDocs(q);
+      // let userPresent = false;
+      // querySnapshot.forEach((doc) => {
+      //   console.log(doc.data());
+      //   // doc.data() is never undefined for query doc snapshots
+      //   // console.log(doc.id, " => ", doc.data().currentSong);
+      //   if (doc.data().members.includes(userName)) {
+      //     userPresent = true;
+      //     console.log("IS PRESENT");
+      //   }
+      // });
+      let userPresent = false;
+      if (foundParty.data().members.includes(userName)) {
+        userPresent = true;
+        console.log("User is already present in the party.");
+      }
+      if (!userPresent) {
+        // const docRef = await addDoc(collection(projDB, "users"), userName);
+        let copyData = foundParty.data();
+        copyData.members.push(userName);
+        copyData.numberActive = copyData.members.length;
+
+        const docNeededId = foundParty.id;
+        const updatePartyRef = doc(projDB, "parties", docNeededId);
+        // console.log(foundParty.id);
+        await setDoc(updatePartyRef, copyData);
+      }
+      window.location.href = "/party/" + partyCode;
+      // Navigate to the party dashboard page
+    } else {
       let errorFinding = "Party Not Found!";
       console.log(errorFinding);
       alert(errorFinding);
     }
-
-  }
+  };
 
   return (
     <Container
@@ -133,7 +160,7 @@ const Landing = () => {
       style={{ height: "100vh " }}
     >
       <Row>
-        <p>Welcome ,{user?.displayName}</p>
+        <p>Welcome, {user?.displayName}</p>
       </Row>
       <br></br>
       <Row>
@@ -158,10 +185,11 @@ const Landing = () => {
               onChange={(e) => {
                 setEntered(e.target.value);
               }}
+              value={entered}
             />
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" onClick={getParty}>
             Join the Session!
           </Button>
         </Form>
